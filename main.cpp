6f2894fc9beeec266583249738bb78c6f515c29f
MinhVKhanh <minhvu933205@gmail.com>
2023-12-01 19:36:36 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,95 +1,156 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int space[3][3]= {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+char space[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 int row;
 int column;
-char token= 'x';
+bool tie = false;
+char token = 'x';
+string n1 = " ";
+string n2 = " ";
 
-void functionone{
+void functionOne()
+{
 
-    string n1 ;
-    string n2 ;
-    cout<<"first player \n";
-    getline(cin, n1);
-    cout<<"second player : \n";
-    getline(cin, n2);
-    cout<<n1<<" play first \n";
-
-    cout<<"     |     |    \n";
-    cout<<" "<<space[0][0]<<"   | "<<space[0][1]<<"   | "<<space[0][2]<<"\n";
-    cout<<"_____|_____|_____\n";
-    cout<<"     |     |    \n";
-    cout<<" "<<space[1][0]<<"   | "<<space[1][1]<<"   | "<<space[1][2]<<"\n";
-    cout<<"_____|_____|______\n";
-    cout<<"     |     |    \n";
-    cout<<" "<<space[2][0]<<"   | "<<space[2][1]<<"   | "<<space[2][2]<<"\n";
-    cout<<"     |     |    \n";
-
+    cout << "     |     |    \n";
+    cout << " " << space[0][0] << "   | " << space[0][1] << "   | " << space[0][2] << "\n";
+    cout << "_____|_____|_____\n";
+    cout << "     |     |    \n";
+    cout << " " << space[1][0] << "   | " << space[1][1] << "   | " << space[1][2] << "\n";
+    cout << "_____|_____|______\n";
+    cout << "     |     |    \n";
+    cout << " " << space[2][0] << "   | " << space[2][1] << "   | " << space[2][2] << "\n";
+    cout << "     |     |    \n";
 }
-void functionTwo{
+void functionTwo()
+{
     int digit;
-    if (token=='x'){
-        cout<<n1<<"please enter";
-        cin>>digit;
+    if (token == 'x')
+    {
+        cout << n1 << "please enter";
+        cin >> digit;
     }
-    if (token=='0'){
-        cout<<n2<<"please enter";
-        cin>>digit;
+    if (token == '0')
+    {
+        cout << n2 << "please enter";
+        cin >> digit;
     }
-    if (digit==1){
-        row=0;
-        column=0;
+    if (digit == 1)
+    {
+        row = 0;
+        column = 0;
     }
-    if (digit==2){
-        row=0;
-        column=1;
+    if (digit == 2)
+    {
+        row = 0;
+        column = 1;
     }
-    if (digit==3){
-        row=0;
-        column=2;
-    }if (digit==4){
-        row=1;
-        column=0;
-    }if (digit==5){
-        row=1;
-        column=1;
-    }if (digit==6){
-        row=1;
-        column=2;
-    }if (digit==7){
-        row=2;
-        column=0;
-    }if (digit==8){
-        row=2;
-        column=1;
-    }if (digit==9){
-        row=2;
-        column=2;
+    if (digit == 3)
+    {
+        row = 0;
+        column = 2;
     }
-    else{
-        cout<<"Invalid"<<endl;
+    if (digit == 4)
+    {
+        row = 1;
+        column = 0;
     }
-    if (token=='x' && space[row][column] !='x' && space[row][column] !='0'){
-        space[row][column]='x';
-        token='0';
+    if (digit == 5)
+    {
+        row = 1;
+        column = 1;
     }
-    else if (token=='0' && space[row][column] && space[row][column] !='0'){
-        space[row][column]='0';
-        token='x';
+    if (digit == 6)
+    {
+        row = 1;
+        column = 2;
     }
-    else {
-        cout<<"these is no empty space!"<<endl;
+    if (digit == 7)
+    {
+        row = 2;
+        column = 0;
+    }
+    if (digit == 8)
+    {
+        row = 2;
+        column = 1;
+    }
+    if (digit == 9)
+    {
+        row = 2;
+        column = 2;
+    }
+    else if (digit < 1 || digit > 9)
+    {
+        cout << "Invalid" << endl;
+    }
+    if (token == 'x' && space[row][column] != 'x' && space[row][column] != '0')
+    {
+        space[row][column] = 'x';
+        token = '0';
+    }
+    else if (token == '0' && space[row][column] && space[row][column] != '0')
+    {
+        space[row][column] = '0';
+        token = 'x';
+    }
+    else
+    {
+        cout << "these is no empty space!" << endl;
         functionTwo();
-    }
-
-    bool functionThree(){
-        for (int i=0;i<3;i++){
-            
+        {
+            functionOne();
         }
     }
+}
+bool functionThree()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (space[i][0] == space[i][1] && space[i][1] == space[i][2] || space[0][i] == space[1][i] && space[1][i] == space[2][1])
+            return true;
+    }
+    if (space[0][0] == space[1][1] && space[1][1] == space[2][2] || space[0][2] == space[1][1] && space[1][1] == space[2][0])
+    {
+        return true;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (space[i][j] != 'x' && space[i][j] != '0')
+            {
+                return false;
+            }
+        }
+    }
+    tie = true;
+    return false;
+}
 
-    
-
-
+int main()
+{
+    cout << "first player \n";
+    getline(cin, n1);
+    cout << "second player : \n";
+    getline(cin, n2);
+    cout << n1 << " play first \n";
+    while (!functionThree())
+    {
+        functionOne();
+        functionTwo();
+        functionThree();
+    }
+    if (token == 'x' && tie == false)
+    {
+        cout << n2 << "Win!" << endl;
+    }
+    else if (token == '0' && tie == false)
+    {
+        cout << n1 << "Win!" << endl;
+    }
+    else
+    {
+        cout << "Draw!" << endl;
+    }
 }
